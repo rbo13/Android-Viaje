c37a5b3948af72290541d9da5860655ccd0571a9
whaangbuu <whaangbuu@github.com>
2017-01-18 18:05:28 +0800

    Added delete module on user logout

--- app/src/main/java/com/app/viaje/viaje/MainActivity.java
+++ app/src/main/java/com/app/viaje/viaje/MainActivity.java
@@ -5,18 +5,25 @@ import android.content.Context;
 import android.content.DialogInterface;
 import android.content.Intent;
 import android.content.SharedPreferences;
+import android.graphics.Color;
 import android.location.Location;
 import android.location.LocationListener;
 import android.location.LocationManager;
 import android.os.Bundle;
+import android.os.Handler;
 import android.support.annotation.NonNull;
 import android.support.annotation.Nullable;
+import android.support.design.widget.CoordinatorLayout;
+import android.support.design.widget.Snackbar;
 import android.support.v7.app.AlertDialog;
 import android.support.v7.app.AppCompatActivity;
 import android.util.Log;
 import android.view.Menu;
 import android.view.MenuItem;
+import android.view.View;
 import android.widget.EditText;
+import android.widget.RelativeLayout;
+import android.widget.TextView;
 import android.widget.Toast;
 
 import com.google.android.gms.common.ConnectionResult;
@@ -45,8 +52,13 @@ import models.OnlineUser;
 public class MainActivity extends AppCompatActivity implements GoogleApiClient.ConnectionCallbacks,
             GoogleApiClient.OnConnectionFailedListener, com.google.android.gms.location.LocationListener{
 
+    Handler handler;
+
     //Checks user in-activity.
     private Timer timer;
+
+    //For snackbar
+    private CoordinatorLayout coordinatorLayout;
 
     private FirebaseAuth mFirebaseAuth;
     private FirebaseUser mFirebaseUser;
@@ -74,6 +86,8 @@ public class MainActivity extends AppCompatActivity implements GoogleApiClient.C
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        coordinatorLayout = (CoordinatorLayout) findViewById(R.id.coordinatorLayout);
 
         buildGoogleApiClient();
 
@@ -248,9 +262,20 @@ public class MainActivity extends AppCompatActivity implements GoogleApiClient.C
 
         /**
          * Update the single login user
-         * at firebase "online_users" record.
+         * at firebase "online_users" record at
+         * a give time of 1 minute.
          */
-        updateUserLocationOnLocationChanged(lat, lon);
+        if(mFirebaseUser != null){
+
+            new android.os.Handler().postDelayed(new Runnable() {
+                @Override
+                public void run() {
+                    updateUserLocationOnLocationChanged(lat, lon);
+                }
+            }, 60000);
+
+        }
+
     }
 
     private void updateUserLocationOnLocationChanged(final String string_latitude, final String string_longitude) {
@@ -264,26 +289,74 @@ public class MainActivity extends AppCompatActivity implements GoogleApiClient.C
         dbRef = FirebaseDatabase.getInstance().getReference()
                 .child(ViajeConstants.ONLINE_USERS_KEY);
 
+        if(mFirebaseUser != null){
+
+            dbRef.orderByChild(ViajeConstants.MOTORIST_EMAIL_ADDRESS_KEY)
+                    .equalTo(email_address)
+                    .addListenerForSingleValueEvent(new ValueEventListener() {
+                        @Override
+                        public void onDataChange(DataSnapshot dataSnapshot) {
+
+                            String key = "";
+
+                            for (DataSnapshot nodeDataSnapshot : dataSnapshot.getChildren()){
+                                key = nodeDataSnapshot.getKey();
+                            }
+
+                            HashMap<String, Object> updated_online_user = new HashMap<>();
+                            updated_online_user.put("latitude", latitude);
+                            updated_online_user.put("longitude", longitude);
+
+                            dbRef.child(key).updateChildren(updated_online_user);
+
+                        }
+
+                        @Override
+                        public void onCancelled(DatabaseError databaseError) {
+                            Log.e("ERROR:", "onCancelled", databaseError.toException());
+                        }
+                    });
+
+        }
+
+
+    }
+
+    private void deleteUserRecordOnFirebase() {
+
+        SharedPreferences sharedPreferences = getSharedPreferences("motoristInfo", Context.MODE_PRIVATE);
+        String email_address = sharedPreferences.getString("email", "");
+
+        dbRef = FirebaseDatabase.getInstance().getReference()
+                .child(ViajeConstants.ONLINE_USERS_KEY);
+
         dbRef.orderByChild(ViajeConstants.MOTORIST_EMAIL_ADDRESS_KEY)
                 .equalTo(email_address)
                 .addListenerForSingleValueEvent(new ValueEventListener() {
                     @Override
                     public void onDataChange(DataSnapshot dataSnapshot) {
+
                         DataSnapshot nodeDataSnapshot = dataSnapshot.getChildren().iterator().next();
                         String key = nodeDataSnapshot.getKey();
 
-                        HashMap<String, Object> updated_online_user = new HashMap<>();
-                        updated_online_user.put("latitude", latitude);
-                        updated_online_user.put("longitude", longitude);
+                        dbRef.child(key).removeValue(new DatabaseReference.CompletionListener() {
+                            @Override
+                            public void onComplete(DatabaseError databaseError, DatabaseReference databaseReference) {
 
-                        dbRef.child(key).updateChildren(updated_online_user);
+                                if(databaseError == null){
+                                    mFirebaseAuth.signOut();
+                                    loadLoginView();
+                                }
+                            }
+                        });
                     }
 
                     @Override
                     public void onCancelled(DatabaseError databaseError) {
-
+                        Log.e("ERROR:", "onCancelled", databaseError.toException());
                     }
                 });
+
     }
 
     /**
@@ -347,13 +420,23 @@ public class MainActivity extends AppCompatActivity implements GoogleApiClient.C
      */
     @OnClick(R.id.logout_button)
     void onLogout(){
-        mFirebaseAuth.signOut();
-        loadLoginView();
+        /**
+         * Delete the logged in user
+         * to "online_users" record
+         * at firebase.
+         */
+        deleteUserRecordOnFirebase();
     }
 
     @OnClick(R.id.emergency_button_id)
     void onEmergencyClick(){
-        Toast.makeText(getApplicationContext(), "Send Emergency Help", Toast.LENGTH_LONG).show();
+
+        Snackbar snackbar = Snackbar.make(coordinatorLayout, "Help is coming!", Snackbar.LENGTH_LONG);
+        View sbView = snackbar.getView();
+        TextView textView = (TextView) sbView.findViewById(android.support.design.R.id.snackbar_text);
+        textView.setTextColor(Color.RED);
+
+        snackbar.show();
 
         sendEmergencyHelp();
     }
