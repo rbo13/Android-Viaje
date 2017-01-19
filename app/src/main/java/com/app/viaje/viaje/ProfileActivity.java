d921432ff1d108ba8349b1b528c56ebad2b14aa0
whaangbuu <whaangbuu@github.com>
2017-01-19 18:47:00 +0800

    Added profile-screen

--- app/src/main/java/com/app/viaje/viaje/ProfileActivity.java
+++ app/src/main/java/com/app/viaje/viaje/ProfileActivity.java
@@ -1,13 +1,91 @@
 package com.app.viaje.viaje;
 
+import android.content.Context;
+import android.content.Intent;
+import android.content.SharedPreferences;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.util.Log;
+
+import com.google.firebase.auth.FirebaseAuth;
+import com.google.firebase.auth.FirebaseUser;
+import com.google.firebase.database.DataSnapshot;
+import com.google.firebase.database.DatabaseError;
+import com.google.firebase.database.DatabaseReference;
+import com.google.firebase.database.FirebaseDatabase;
+import com.google.firebase.database.ValueEventListener;
+
+import butterknife.ButterKnife;
+import butterknife.OnClick;
+import helpers.ViajeConstants;
 
 public class ProfileActivity extends AppCompatActivity {
+
+
+    private FirebaseAuth mFirebaseAuth;
+    private FirebaseUser mFirebaseUser;
+    private DatabaseReference dbRef;
+
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_profile);
+
+        mFirebaseAuth = FirebaseAuth.getInstance();
+        mFirebaseUser = mFirebaseAuth.getCurrentUser();
+        dbRef = FirebaseDatabase.getInstance().getReference();
+
+        ButterKnife.bind(this);
+    }
+
+    private void deleteUserRecordOnFirebase() {
+
+        SharedPreferences sharedPreferences = getSharedPreferences("motoristInfo", Context.MODE_PRIVATE);
+        String email_address = sharedPreferences.getString("email", "");
+
+        dbRef = FirebaseDatabase.getInstance().getReference()
+                .child(ViajeConstants.ONLINE_USERS_KEY);
+
+        dbRef.orderByChild(ViajeConstants.MOTORIST_EMAIL_ADDRESS_KEY)
+                .equalTo(email_address)
+                .addListenerForSingleValueEvent(new ValueEventListener() {
+                    @Override
+                    public void onDataChange(DataSnapshot dataSnapshot) {
+
+                        DataSnapshot nodeDataSnapshot = dataSnapshot.getChildren().iterator().next();
+                        String key = nodeDataSnapshot.getKey();
+
+                        dbRef.child(key).removeValue(new DatabaseReference.CompletionListener() {
+                            @Override
+                            public void onComplete(DatabaseError databaseError, DatabaseReference databaseReference) {
+
+                                if(databaseError == null){
+                                    mFirebaseAuth.signOut();
+                                    loadLoginView();
+                                }
+                            }
+                        });
+                    }
+
+                    @Override
+                    public void onCancelled(DatabaseError databaseError) {
+                        Log.e("ERROR:", "onCancelled", databaseError.toException());
+                    }
+                });
+
+    }
+
+    private void loadLoginView(){
+        Intent intent = new Intent(this, LogInActivity.class);
+        intent.addFlags(Intent.FLAG_ACTIVITY_NEW_TASK);
+        intent.addFlags(Intent.FLAG_ACTIVITY_CLEAR_TASK);
+        startActivity(intent);
+    }
+
+    //Butterknife components
+    @OnClick(R.id.logout_button)
+    void onLogout(){
+        deleteUserRecordOnFirebase();
     }
 }
