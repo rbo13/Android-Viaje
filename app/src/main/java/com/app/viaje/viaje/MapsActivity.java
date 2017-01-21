f8fdac4baf43b1a272d4da8a99f175aa5f91fdaa
whaangbuu <whaangbuu@github.com>
2017-01-21 15:17:46 +0800

    Still fixing for querying of safezones and display it to map.

--- app/src/main/java/com/app/viaje/viaje/MapsActivity.java
+++ app/src/main/java/com/app/viaje/viaje/MapsActivity.java
@@ -2,8 +2,10 @@ package com.app.viaje.viaje;
 
 import android.content.Context;
 import android.content.SharedPreferences;
+import android.graphics.Color;
 import android.support.v4.app.FragmentActivity;
 import android.os.Bundle;
+import android.support.v4.content.ContextCompat;
 import android.widget.Toast;
 
 import com.google.android.gms.maps.CameraUpdateFactory;
@@ -11,13 +13,26 @@ import com.google.android.gms.maps.GoogleMap;
 import com.google.android.gms.maps.MapsInitializer;
 import com.google.android.gms.maps.OnMapReadyCallback;
 import com.google.android.gms.maps.SupportMapFragment;
+import com.google.android.gms.maps.model.BitmapDescriptorFactory;
+import com.google.android.gms.maps.model.CircleOptions;
 import com.google.android.gms.maps.model.LatLng;
 import com.google.android.gms.maps.model.Marker;
 import com.google.android.gms.maps.model.MarkerOptions;
+import com.google.firebase.database.DataSnapshot;
+import com.google.firebase.database.DatabaseError;
+import com.google.firebase.database.DatabaseReference;
+import com.google.firebase.database.FirebaseDatabase;
+import com.google.firebase.database.Query;
+import com.google.firebase.database.ValueEventListener;
+
+import helpers.ViajeConstants;
+import models.Safezone;
 
 public class MapsActivity extends FragmentActivity implements OnMapReadyCallback {
 
     private GoogleMap mMap;
+
+    private DatabaseReference dbRef;
 
     GPSTracker gps;
 
@@ -28,6 +43,8 @@ public class MapsActivity extends FragmentActivity implements OnMapReadyCallback
         // Obtain the SupportMapFragment and get notified when the map is ready to be used.
         SupportMapFragment mapFragment = (SupportMapFragment) getSupportFragmentManager()
                 .findFragmentById(R.id.map);
+
+        dbRef = FirebaseDatabase.getInstance().getReference();
         mapFragment.getMapAsync(this);
     }
 
@@ -66,10 +83,12 @@ public class MapsActivity extends FragmentActivity implements OnMapReadyCallback
          * base on GPS Location and apply it
          * to the map.
          */
+        currentUserLocation(googleMap);
+        getSafezones(googleMap);
 //        setUpMapPin(googleMap);
     }
 
-    private void setUpMapPin(GoogleMap googleMap){
+    private void currentUserLocation(GoogleMap googleMap) {
 
         mMap = googleMap;
 
@@ -78,12 +97,108 @@ public class MapsActivity extends FragmentActivity implements OnMapReadyCallback
         double latitude = Double.parseDouble(sharedPreferences.getString("latitude", ""));
         double longitude = Double.parseDouble(sharedPreferences.getString("longitude", ""));
 
-        Toast.makeText(MapsActivity.this, "Latitude: "+ latitude + " : " + "Longitude: " +longitude, Toast.LENGTH_LONG).show();
-
         LatLng location = new LatLng(latitude, longitude); // User Current Location
-        mMap.addMarker(new MarkerOptions().position(location).title("Current Location"));
+
+        mMap.addMarker(new MarkerOptions().position(location)
+                .title("Current Location")
+                .icon(BitmapDescriptorFactory.fromResource(R.drawable.motorist)));
+
+        mMap.addCircle(drawCircle(location));
+
         mMap.moveCamera(CameraUpdateFactory.newLatLng(location));
-        mMap.moveCamera(CameraUpdateFactory.newLatLngZoom(location, 16));
+        mMap.moveCamera(CameraUpdateFactory.newLatLngZoom(location, 14));
+    }
+
+    private void getSafezones(GoogleMap googleMap) {
+
+        mMap = googleMap;
+
+        Query queryRef = dbRef.child(ViajeConstants.USERS_KEY)
+                .orderByChild(ViajeConstants.TYPE_FIELD)
+                .equalTo(ViajeConstants.SAFEZONE);
+
+        queryRef.addListenerForSingleValueEvent(new ValueEventListener() {
+            @Override
+            public void onDataChange(DataSnapshot dataSnapshot) {
+
+                for (DataSnapshot safezoneSnapshot : dataSnapshot.getChildren()){
+
+                    Safezone safezone = safezoneSnapshot.getValue(Safezone.class);
+
+                    double latitude = safezone.getAddress().getLatitude();
+                    double longitude = safezone.getAddress().getLongitude();
+                    String address = safezone.getAddress().getAddress();
+                    String contact_number = safezone.getContact_number();
+                    String email_address = safezone.getEmail_address();
+                    String owner = safezone.getOwner();
+                    String service_information_type = safezone.getService_information_type();
+                    String shop_name = safezone.getShop_name();
+                    String type = safezone.getType();
+                    String username = safezone.getUsername();
+
+                    /**
+                     * Create marker in maps
+                     * with type of safezone.
+                     */
+                    Toast.makeText(MapsActivity.this, "Create The Pins Now!", Toast.LENGTH_LONG).show();
+                    LatLng safezone_location = new LatLng(latitude, longitude); // Safezone Current Location
+
+                    if(service_information_type.contains("repair")){
+
+                        mMap.addMarker(new MarkerOptions().position(safezone_location)
+                                .title(shop_name)
+                                .snippet(owner)
+                                .icon(BitmapDescriptorFactory.fromResource(R.drawable.repair)));
+
+
+                    }else if(service_information_type.contains("gasoline")){
+
+                        mMap.addMarker(new MarkerOptions().position(safezone_location)
+                                .title(shop_name)
+                                .snippet(owner)
+                                .icon(BitmapDescriptorFactory.fromResource(R.drawable.gasoline)));
+
+                    }else if(service_information_type.contains("police")){
+
+                        mMap.addMarker(new MarkerOptions().position(safezone_location)
+                                .title(shop_name)
+                                .snippet(owner)
+                                .icon(BitmapDescriptorFactory.fromResource(R.drawable.police)));
+
+                    }else if(service_information_type.contains("hospital")){
+
+                        mMap.addMarker(new MarkerOptions().position(safezone_location)
+                                .title(shop_name)
+                                .snippet(owner)
+                                .icon(BitmapDescriptorFactory.fromResource(R.drawable.hospital)));
+
+                    }else if(service_information_type.contains("towing")){
+
+                        mMap.addMarker(new MarkerOptions().position(safezone_location)
+                                .title(shop_name)
+                                .snippet(owner)
+                                .icon(BitmapDescriptorFactory.fromResource(R.drawable.towing)));
+
+
+                    }else if(service_information_type.contains("vulcanizing")){
+
+                        mMap.addMarker(new MarkerOptions().position(safezone_location)
+                                .title(shop_name)
+                                .snippet(owner)
+                                .icon(BitmapDescriptorFactory.fromResource(R.drawable.vulcanizing)));
+                    }
+
+
+                }
+
+            }
+
+            @Override
+            public void onCancelled(DatabaseError databaseError) {
+
+            }
+        });
+
     }
 
     private void initMap(){
@@ -98,7 +213,8 @@ public class MapsActivity extends FragmentActivity implements OnMapReadyCallback
             mapFragment.getMapAsync(new OnMapReadyCallback() {
                 @Override
                 public void onMapReady(GoogleMap googleMap) {
-                    setUpMapPin(googleMap);
+                    currentUserLocation(googleMap);
+                    getSafezones(googleMap);
                 }
             });
         }else{
@@ -111,5 +227,18 @@ public class MapsActivity extends FragmentActivity implements OnMapReadyCallback
     private void clearPin(){
 
         mMap.clear();
+    }
+
+    private CircleOptions drawCircle(LatLng location){
+
+        CircleOptions options = new CircleOptions();
+
+        options.center(location);
+        options.radius(1500);
+        options.fillColor(Color.argb(10, 0, R.color.app_color, 0));
+        options.strokeColor(ContextCompat.getColor(getApplicationContext(), R.color.stroke_color));
+        options.strokeWidth(5);
+
+        return options;
     }
 }
--- app/src/main/java/helpers/ViajeConstants.java
+++ app/src/main/java/helpers/ViajeConstants.java
@@ -15,6 +15,9 @@ public class ViajeConstants {
     public static final String EMAIL_ADDRESS_FIELD = "email_address";
     public static final String MOTORIST_FIELD = "motorist";
 
+    public static final String TYPE_FIELD = "type";
+    public static final String SAFEZONE = "safezone";
+
     /**
      * URLS:
      */
